a0fd52f0abf3d76648b5fcc7ec42cb4104dd2a32
A. J. Wright <ajw@toadking.org>
2023-07-01 19:30:06 -0400

    draw a window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use bevy::{
+    diagnostic::{
+        FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin, SystemInformationDiagnosticsPlugin,
+    },
+    prelude::*,
+};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(LogDiagnosticsPlugin::default())
+        .add_plugin(FrameTimeDiagnosticsPlugin::default())
+        .run();
 }
