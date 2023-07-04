fd1e7ed3695903e1fbc6c59308781b97f889703a
A. J. Wright <ajw@toadking.org>
2023-07-04 19:22:09 -0400

    initial bevy_ecs_ldtk setup

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,25 @@ use bevy::{
     },
     prelude::*,
 };
+use bevy_ecs_ldtk::prelude::*;
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(LdtkWorldBundle {
+        ldtk_handle: asset_server.load("exterminator_wizard.ldtk"),
+        ..Default::default()
+    });
+}
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(
+            DefaultPlugins.set(ImagePlugin::default_nearest()), // prevents blurry sprites
+        )
         .add_plugin(LogDiagnosticsPlugin::default())
+        .add_plugin(LdtkPlugin)
         .add_plugin(FrameTimeDiagnosticsPlugin::default())
+        .add_plugin(SystemInformationDiagnosticsPlugin::default())
+        .add_startup_system(setup)
         .run();
 }
