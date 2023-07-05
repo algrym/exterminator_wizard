77f265c38321535f03cdd2efc667dcc2fd2dc721
A. J. Wright <ajw@toadking.org>
2023-07-04 20:45:09 -0400

    Load a random example LDTK file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use bevy::{
     diagnostic::{
         FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin, SystemInformationDiagnosticsPlugin,
@@ -6,10 +8,27 @@ use bevy::{
 };
 use bevy_ecs_ldtk::prelude::*;
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+//  egrep '\t"levelIid"' assets/exterminator_wizard.ldtk | awk '{ print $2 }'
+const LEVEL_IIDS: [&str; 4] = [
+    "a316bd80-66b0-11ec-9cd7-c50cdc9d2cc4",
+    "a317cef0-66b0-11ec-9cd7-dd2f249c8c8b",
+    "a315ac10-66b0-11ec-9cd7-99f223ad6ade",
+    "a315ac10-66b0-11ec-9cd7-99f223ad6ade",
+];
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>, windows: Query<&Window>) {
     commands.spawn(Camera2dBundle::default());
+
+    let iids: HashSet<String> = LEVEL_IIDS.into_iter().map(|s| s.to_string()).collect();
+    let window = windows.single();
+    info!("Window size: {},{}", window.width(), window.height());
+
     commands.spawn(LdtkWorldBundle {
         ldtk_handle: asset_server.load("exterminator_wizard.ldtk"),
+        level_set: LevelSet { iids },
+        transform: Transform::from_xyz(window.width() / -2.0,
+                                       window.height() / -2.0,
+                                       0.0),
         ..Default::default()
     });
 }
@@ -20,9 +39,17 @@ fn main() {
             DefaultPlugins.set(ImagePlugin::default_nearest()), // prevents blurry sprites
         )
         .add_plugin(LogDiagnosticsPlugin::default())
-        .add_plugin(LdtkPlugin)
         .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .add_plugin(SystemInformationDiagnosticsPlugin::default())
+        .add_plugin(LdtkPlugin)
         .add_startup_system(setup)
+        .insert_resource(LdtkSettings {
+            // By default, levels are just spawned at the origin of the world.
+            // This makes them spawn according to their location in LDtk
+            level_spawn_behavior: LevelSpawnBehavior::UseWorldTranslation {
+                load_level_neighbors: true,
+            },
+            ..Default::default()
+        })
         .run();
 }
