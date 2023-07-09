2bcb5aaf408fa0524b3aee3bc46706a2d0648a10
A. J. Wright <ajw@toadking.org>
2023-07-09 19:03:13 -0400

    Checkpoint before rework.

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,9 @@ use bevy::{
     },
     prelude::*,
 };
-use bevy::utils::tracing::Instrument;
 use bevy_ecs_ldtk::prelude::*;
+
+pub mod player;
 
 //  egrep '\t"levelIid"' assets/exterminator_wizard.ldtk | awk '{ print $2 }'
 const LEVEL_IIDS: [&str; 4] = [
@@ -17,7 +18,60 @@ const LEVEL_IIDS: [&str; 4] = [
     "a315ac10-66b0-11ec-9cd7-99f223ad6ade",
 ];
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>, windows: Query<&Window>) {
+
+#[derive(Component)]
+struct AnimationIndices {
+    first: usize,
+    last: usize,
+}
+
+#[derive(Component, Deref, DerefMut)]
+struct AnimationTimer(Timer);
+
+fn animate_sprite(
+    time: Res<Time>,
+    mut query: Query<(
+        &AnimationIndices,
+        &mut AnimationTimer,
+        &mut TextureAtlasSprite,
+    )>,
+) {
+    for (indices, mut timer, mut sprite) in &mut query {
+        timer.tick(time.delta());
+        if timer.just_finished() {
+            sprite.index = if sprite.index == indices.last {
+                indices.first
+            } else {
+                sprite.index + 1
+            };
+        }
+    }
+}
+
+pub fn setup_player (
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    let texture_handle = asset_server.load("textures/rpg/chars/gabe/gabe-idle-run.png");
+    let texture_atlas =
+        TextureAtlas::from_grid(texture_handle, Vec2::new(24.0, 24.0), 7, 1, None, None);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+    // Use only the subset of sprites in the sheet that make up the run animation
+    let animation_indices = AnimationIndices { first: 1, last: 6 };
+    commands.spawn((
+        SpriteSheetBundle {
+            texture_atlas: texture_atlas_handle,
+            sprite: TextureAtlasSprite::new(animation_indices.first),
+            transform: Transform::from_scale(Vec3::splat(6.0)),
+            ..default()
+        },
+        animation_indices,
+        AnimationTimer(Timer::from_seconds(0.1, TimerMode::Repeating)),
+    ));
+}
+
+fn setup_map(mut commands: Commands, asset_server: Res<AssetServer>, windows: Query<&Window>) {
     let mut camera = Camera2dBundle::default();
     camera.projection.scale /= 2.0;
     commands.spawn(camera);
@@ -42,11 +96,12 @@ fn main() {
         .add_plugins(
             DefaultPlugins.set(ImagePlugin::default_nearest()), // prevents blurry sprites
         )
+
         .add_plugin(LogDiagnosticsPlugin::default())
         .add_plugin(FrameTimeDiagnosticsPlugin::default())
         .add_plugin(SystemInformationDiagnosticsPlugin::default())
-        .add_plugin(LdtkPlugin)
-        .add_startup_system(setup)
+
+        .add_startup_system(setup_map)
         .insert_resource(LdtkSettings {
             level_spawn_behavior: LevelSpawnBehavior::UseWorldTranslation {
                 load_level_neighbors: true,
@@ -54,5 +109,9 @@ fn main() {
             set_clear_color: SetClearColor::FromLevelBackground,
             ..Default::default()
         })
+        .add_plugin(LdtkPlugin)
+
+        .add_startup_system(setup_player)
+        .add_system(animate_sprite)
         .run();
 }
