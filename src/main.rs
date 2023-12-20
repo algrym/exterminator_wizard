752f4febcff406f13f6e176c685b607b322aa7c9
A. J. Wright <ajw@toadking.org>
2023-12-19 20:42:35 -0500

    Integrate PanCamPlugin

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
-use bevy::{
-    prelude::*,
-};
+use bevy::prelude::*;
 use bevy_ecs_ldtk::prelude::*;
+use bevy_pancam::{PanCam, PanCamPlugin};
 
 // What is the filename of the map to load?
 const MAP_FILENAME: &str = "map.ldtk";
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
-        .add_plugins(LdtkPlugin)
-
+        .add_plugins((DefaultPlugins.set(ImagePlugin::default_nearest()),
+                      LdtkPlugin,
+                      PanCamPlugin::default(),
+        ))
         .insert_resource(LevelSelection::Uid(0))
         .insert_resource(LdtkSettings {
             level_spawn_behavior: LevelSpawnBehavior::UseWorldTranslation {
@@ -25,7 +25,8 @@ fn main() {
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2dBundle::default());
+    commands.spawn(Camera2dBundle::default())
+        .insert(PanCam::default());
 
     commands.spawn(LdtkWorldBundle {
         ldtk_handle: asset_server.load(MAP_FILENAME),
