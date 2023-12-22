1ad0c4ce2c4d146a3a4476780710b5ff7a8f6ae1
A. J. Wright <ajw@toadking.org>
2023-12-21 22:57:08 -0500

    Fix transform and comments

--- src/player.rs
+++ src/player.rs
@@ -29,22 +29,23 @@ fn setup_player(
     asset_server: Res<AssetServer>,
     mut texture_atlases: ResMut<Assets<TextureAtlas>>,
 ) {
+    // Load the sprite sheet
     let texture_handle = asset_server.load("0x72_DungeonTilesetII_v1.6.png");
-    // Define the coordinates and sizes for each frame of the player's animation
-    // ...
 
+    // Break the sprite sheet down into a texture atlas (indexed grid)
     let texture_atlas =
-        // Adjust the grid size and count based on your sprite sheet
         TextureAtlas::from_grid(texture_handle,
                                 Vec2::new(SPRITE_WIDTH, SPRITE_HEIGHT),
                                 SPRITE_SHEET_SIZE, SPRITE_SHEET_SIZE,
                                 None, None);
     let texture_atlas_handle = texture_atlases.add(texture_atlas);
 
+    // Save the player sprite
     commands
         .spawn(SpriteSheetBundle {
             texture_atlas: texture_atlas_handle,
-            transform: Transform::from_scale(Vec3::splat(2.0)),
+            // Shift the sprite a half-tile horizontally to make it line up
+            transform: Transform::from_xyz(SPRITE_WIDTH / 2.0, 0.0, 0.0),
             ..Default::default()
         })
         .insert(AnimationTimer(Timer::from_seconds(
@@ -61,6 +62,8 @@ fn animate_player(
     for (mut timer, mut sprite) in query.iter_mut() {
         timer.0.tick(time.delta());
         if timer.0.just_finished() {
+            // Keep the sprite index in bounds,
+            //   incrementing if possible.
             sprite.index = if (sprite.index >= PLAYER_SPRITE_LAST_INDEX)
                 || (sprite.index < PLAYER_SPRITE_FIRST_INDEX)
             {
