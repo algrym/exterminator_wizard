ae47b956333dbdf3b8decc8bd7109c8df0a151dd
A. J. Wright <ajw@toadking.org>
2023-12-30 21:28:57 -0500

    More function docs, more or less useful than others

--- src/components.rs
+++ src/components.rs
@@ -4,20 +4,28 @@ use bevy::prelude::{Bundle, Component, SpriteSheetBundle, Timer};
 use bevy::time::TimerMode;
 use bevy_ecs_ldtk::{GridCoords, LdtkEntity, LdtkIntCell};
 
-// use bevy_inspector_egui::InspectorOptions;
-
+/// Plugin responsible for adding player-related systems to the game.
 pub struct PlayerPlugin;
 
+/// Component representing the player entity.
+/// This component is used to identify and interact with the player in the game world.
 #[derive(Default, Component)]
 pub struct Player;
 
+/// Component for handling sprite animation.
+///
+/// Contains a list of frame indices for the animation and a timer to control the
+/// frame rate of the animation.
 #[derive(Component)]
 pub struct Animation {
+    /// Indices of the frames in the sprite sheet used for animation.
     pub frames: Vec<usize>,
+    /// Timer to control when the frame should be updated.
     pub timer: Timer,
 }
 
 impl Default for Animation {
+    /// Provides a default set of frames and a timer for the animation.
     fn default() -> Self {
         Animation {
             frames: vec![136, 137, 138, 139, 140, 141, 142, 143, 144], // TODO: load this elsewhere
@@ -26,6 +34,8 @@ impl Default for Animation {
     }
 }
 
+/// Bundle for creating a player entity.
+/// Groups all necessary components for a player entity, including sprite, grid position, and animation.
 #[derive(Default, Bundle, LdtkEntity)]
 pub struct PlayerBundle {
     pub player: Player,
@@ -36,11 +46,15 @@ pub struct PlayerBundle {
     pub animation: Animation,
 }
 
+/// Plugin responsible for adding map-related systems to the game.
 pub struct MapPlugin;
 
+/// Component representing a wall in the game world.
 #[derive(Default, Component)]
 pub struct Wall;
 
+/// Bundle for creating a wall entity.
+/// Groups all necessary components for a wall entity, primarily used for collision detection.
 #[derive(Default, Bundle, LdtkIntCell)]
 pub struct WallBundle {
     pub wall: Wall,
