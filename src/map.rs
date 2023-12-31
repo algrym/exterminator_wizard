6ae2e397ea1b844fd57ef8e21fc950cea1cde3ba
A. J. Wright <ajw@toadking.org>
2023-12-30 21:14:52 -0500

    Add better docs and unit test

--- src/map.rs
+++ src/map.rs
@@ -6,6 +6,8 @@ use bevy_ecs_ldtk::prelude::*;
 use crate::components::*;
 use crate::constants::*;
 
+/// This plugin is responsible for handling map-related functionalities
+/// in the game, including processing and caching wall locations.
 impl Plugin for MapPlugin {
     fn build(&self, app: &mut App) {
         app.register_ldtk_int_cell::<WallBundle>(1)
@@ -14,6 +16,8 @@ impl Plugin for MapPlugin {
     }
 }
 
+/// This plugin is responsible for handling map-related functionalities
+/// in the game, including processing and caching wall locations.
 #[derive(Default, Resource)]
 pub struct LevelWalls {
     wall_locations: HashSet<GridCoords>,
@@ -22,6 +26,13 @@ pub struct LevelWalls {
 }
 
 impl LevelWalls {
+    /// Checks if the given grid coordinates are within a wall.
+    ///
+    /// # Arguments
+    /// * `grid_coords` - The grid coordinates to check.
+    ///
+    /// # Returns
+    /// `true` if the coordinates are within a wall, `false` otherwise.
     pub fn in_wall(&self, grid_coords: &GridCoords) -> bool {
         grid_coords.x < 0
             || grid_coords.y < 0
@@ -31,6 +42,9 @@ impl LevelWalls {
     }
 }
 
+/// Caches the locations of walls whenever a level is spawned.
+/// This function listens for `LevelEvent::Spawned` events and updates
+/// the `LevelWalls` resource with the wall locations for the current level.
 fn cache_wall_locations(
     mut level_walls: ResMut<LevelWalls>,
     mut level_events: EventReader<LevelEvent>,
@@ -57,5 +71,25 @@ fn cache_wall_locations(
 
             *level_walls = new_level_walls;
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_in_wall() {
+        let mut level_walls = LevelWalls {
+            wall_locations: HashSet::new(),
+            level_width: 10,
+            level_height: 10,
+        };
+        level_walls.wall_locations.insert(GridCoords::new(5, 5));
+
+        assert!(!level_walls.in_wall(&GridCoords::new(1, 1))); // Inside the level and not a wall
+        assert!(level_walls.in_wall(&GridCoords::new(5, 5))); // Wall location
+        assert!(level_walls.in_wall(&GridCoords::new(-1, 0))); // Outside the level boundaries
+        assert!(level_walls.in_wall(&GridCoords::new(10, 10))); // Outside the level boundaries
     }
 }
