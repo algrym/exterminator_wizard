0ba43f96deacb17d8e27efd9a8c9c8d559bcfe00
A. J. Wright <ajw@toadking.org>
2024-01-20 21:11:49 -0500

    More work on collider optimization

--- src/map.rs
+++ src/map.rs
@@ -117,8 +117,9 @@ fn _naive_setup_wall_colliders(
     }
 }
 
+#[allow(clippy::type_complexity)]
 pub fn setup_wall_colliders(
-    mut _commands: Commands,
+    mut commands: Commands,
     wall_query: Query<(&GridCoords, &Parent), Added<Wall>>,
     parent_query: Query<&Parent, Without<Wall>>,
 ) {
@@ -131,7 +132,8 @@ pub fn setup_wall_colliders(
     }
 
     /// A simple rectangle type representing a wall of any size
-    struct _Rect {
+    #[derive(Clone, Eq, PartialEq, Debug, Default)]
+    struct Rect {
         left: i32,
         right: i32,
         top: i32,
@@ -147,8 +149,9 @@ pub fn setup_wall_colliders(
     // 2. it lets us easily add the collision entities as children of the appropriate level entity
     let mut level_to_wall_locations: HashMap<Entity, HashSet<GridCoords>> = HashMap::new();
 
+    let mut grandparent_entity: Option<Entity> = None;
     wall_query.for_each(|(&grid_coords, parent)| {
-        // An intgrid tile's direct parent will be a layer entity, not the level entity
+        // An integer grid tile's direct parent will be a layer entity, not the level entity
         // To get the level entity, you need the tile's grandparent.
         // This is where parent_query comes in.
         if let Ok(grandparent) = parent_query.get(parent.get()) {
@@ -156,6 +159,7 @@ pub fn setup_wall_colliders(
                 .entry(grandparent.get())
                 .or_default()
                 .insert(grid_coords);
+            grandparent_entity = Some(grandparent.get()); // Store the grandparent entity ID for later
         }
     });
 
@@ -170,6 +174,9 @@ pub fn setup_wall_colliders(
                 level_width = level_width.max(wall_location.x);
                 level_height = level_height.max(wall_location.y);
             }
+
+            // combine wall tiles into flat "plates" in each individual row
+            let mut plate_stack: Vec<Vec<Plate>> = Vec::new();
 
             for y in 0..=level_height {
                 let mut left = None;
@@ -193,18 +200,77 @@ pub fn setup_wall_colliders(
                     plates.insert(Plate { left, right });
                 }
             }
-            // log the count of colliders vs original
-            info!(
-                "built {} (down from {}={:.2}%) colliders via plate method",
-                plates.len() * 2,
-                wall_locations.len(),
-                ((plates.len() as f32 * 2.0) / wall_locations.len() as f32) * 100.0
-            );
+
+            // combine "plates" into rectangles across multiple rows
+            let mut rect_builder: HashMap<Plate, Rect> = HashMap::new();
+            let mut prev_row: Vec<Plate> = Vec::new();
+            let mut wall_rects: Vec<Rect> = Vec::new();
+
+            // an extra empty row so the algorithm "finishes" the rects that touch the top edge
+            plate_stack.push(Vec::new());
+
+            for (y, current_row) in plate_stack.into_iter().enumerate() {
+                for prev_plate in &prev_row {
+                    if !current_row.contains(prev_plate) {
+                        // remove the finished rect so that the same plate in the future starts a new rect
+                        if let Some(rect) = rect_builder.remove(prev_plate) {
+                            wall_rects.push(rect);
+                        }
+                    }
+                }
+                for plate in &current_row {
+                    rect_builder
+                        .entry(plate.clone())
+                        .and_modify(|e| e.top += 1)
+                        .or_insert(Rect {
+                            bottom: y as i32,
+                            top: y as i32,
+                            left: plate.left,
+                            right: plate.right,
+                        });
+                }
+                prev_row = current_row;
+            }
 
             // Placeholder so we don't forget
             info!(
                 "TODO: placeholder level_entity={:?} ({},{})",
                 level_entity, WALL_SPRITE_WIDTH, WALL_SPRITE_HEIGHT
+            );
+
+            // Spawn colliders for every rectangle and add them as children of the level entity, stored in grandparent_entity
+            for wall_rect in wall_rects.iter() {
+                if let Some(grandparent_id) = grandparent_entity {
+                    info!(
+                        "new wall_rect={:?} -> entity={:?} id={:?}",
+                        wall_rect, grandparent_entity, grandparent_id
+                    );
+
+                    commands
+                        .entity(grandparent_id)
+                        .insert(Collider::cuboid(
+                            (wall_rect.right as f32 - wall_rect.left as f32 + 1.)
+                                * GRID_SIZE as f32
+                                / 2.,
+                            (wall_rect.top as f32 - wall_rect.bottom as f32 + 1.)
+                                * GRID_SIZE as f32
+                                / 2.,
+                        ))
+                        .insert(RigidBody::Fixed)
+                        .insert(Friction::new(1.0))
+                        .insert(Transform::from_xyz(
+                            (wall_rect.left + wall_rect.right + 1) as f32 * GRID_SIZE as f32 / 2.,
+                            (wall_rect.bottom + wall_rect.top + 1) as f32 * GRID_SIZE as f32 / 2.,
+                            0.,
+                        ));
+                }
+            }
+
+            // log the count of colliders vs original
+            info!(
+                "built {} (from {}) colliders via plate method",
+                wall_rects.len(),
+                wall_locations.len(),
             );
         }
     }
